6c3aa756ff161ab02691c1505259b1fee7899027
Jordi Irawan <jordirwn@gmail.com>
2025-05-14 23:59:03 +0700

    feat: implement CRUD operations for events in admin API

--- src/app/api/[[...route]]/routes/admin.ts
+++ src/app/api/[[...route]]/routes/admin.ts
@@ -61,6 +61,136 @@ app.post('/login', async (c) => {
   });
 });
 
+app.post('/event', async (c) => {
+  const jwtPayload = c.get('jwtPayload') as { sub: number; role: string };
+  const json = await c.req.json<{
+    jenis: 'pendaftaran_asisten' | 'pendaftaran_praktikan' | 'praktikum';
+    mulai: Date;
+    selesai: Date;
+  }>();
+
+  const event = await prisma.event.findFirst({
+    where: {
+      admin_id: jwtPayload.sub,
+      mulai: {
+        gte: new Date(json.mulai),
+      },
+      selesai: {
+        lte: new Date(json.selesai),
+      },
+    },
+  });
+
+  if (event) {
+    return c.json({ status: false, message: 'Event already exists' }, 400);
+  }
+
+  await prisma.event.create({
+    data: {
+      admin_id: jwtPayload.sub,
+      jenis: json.jenis,
+      mulai: new Date(json.mulai),
+      selesai: new Date(json.selesai),
+    },
+  });
+
+  return c.json({
+    status: true,
+  });
+});
+
+app.get('/event', async (c) => {
+  const jwtPayload = c.get('jwtPayload') as { sub: number; role: string };
+
+  const events = await prisma.event.findMany({
+    where: {
+      admin_id: jwtPayload.sub,
+    },
+  });
+
+  return c.json({
+    status: true,
+    data: events,
+  });
+});
+
+app.delete('/event/:id', async (c) => {
+  const jwtPayload = c.get('jwtPayload') as { sub: number; role: string };
+  const eventId = c.req.param('id');
+  const event = await prisma.event.findFirst({
+    where: {
+      id: Number(eventId),
+      admin_id: jwtPayload.sub,
+    },
+  });
+  if (!event) {
+    return c.json({ status: false, message: 'Event not found' }, 404);
+  }
+
+  await prisma.event.delete({
+    where: {
+      id: Number(eventId),
+    },
+  });
+
+  return c.json({
+    status: true,
+  });
+});
+
+app.put('/event', async (c) => {
+  const jwtPayload = c.get('jwtPayload') as { sub: number; role: string };
+  const json = await c.req.json<{
+    event_id: number;
+    jenis: 'pendaftaran_asisten' | 'pendaftaran_praktikan' | 'praktikum';
+    mulai: Date;
+    selesai: Date;
+  }>();
+  let event;
+  event = await prisma.event.findFirst({
+    where: {
+      id: Number(json.event_id),
+      admin_id: jwtPayload.sub,
+    },
+  });
+  if (!event) {
+    return c.json({ status: false, message: 'Event not found' }, 404);
+  }
+  event = await prisma.event.findFirst({
+    where: {
+      id: {
+        not: Number(json.event_id),
+      },
+      admin_id: jwtPayload.sub,
+      mulai: {
+        gte: new Date(json.mulai),
+      },
+      selesai: {
+        lte: new Date(json.selesai),
+      },
+    },
+  });
+
+  if (event) {
+    return c.json({ status: false, message: 'Event already exists' }, 400);
+  }
+
+  await prisma.event.update({
+    where: {
+      id: Number(json.event_id),
+    },
+    data: {
+      jenis: json.jenis,
+      mulai: new Date(json.mulai),
+      selesai: new Date(json.selesai),
+    },
+  });
+
+  return c.json({
+    status: true,
+  });
+});
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
