18d24c6e722a21f21c7c4de332e8be59876f2cf5
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-05-15 19:22:03 +0700

    feat: add CRUD operations for kelas management in admin API

--- src/app/api/[[...route]]/routes/admin.ts
+++ src/app/api/[[...route]]/routes/admin.ts
@@ -10,7 +10,7 @@ import { handle } from 'hono/vercel';
 type JWTPayload = jwtTypes.JWTPayload & {
   sub: number;
   role: 'admin' | 'asisten' | 'praktikan';
-}
+};
 
 export const app = new Hono().basePath('/admin');
 
@@ -25,13 +25,16 @@ app.use(
   }),
 );
 
-app.use('/*', except('*/*/login', async (c, next) => {
-  const jwtPayload = c.get('jwtPayload') as JWTPayload;
-  if (jwtPayload.role !== 'admin') {
-    return c.json({ status: false, message: 'Unauthorized' }, 401);
-  }
-  return next();
-}));
+app.use(
+  '/*',
+  except('*/*/login', async (c, next) => {
+    const jwtPayload = c.get('jwtPayload') as JWTPayload;
+    if (jwtPayload.role !== 'admin') {
+      return c.json({ status: false, message: 'Unauthorized' }, 401);
+    }
+    return next();
+  }),
+);
 
 app.get('/echo', (c) => {
   return c.json({ message: 'Hello from admin!' });
@@ -249,9 +252,12 @@ app.post('/ruangan', async (c) => {
     },
   });
 
-  return c.json({
-    status: true,
-  }, 201);
+  return c.json(
+    {
+      status: true,
+    },
+    201,
+  );
 });
 
 app.put('/ruangan', async (c) => {
@@ -338,6 +344,160 @@ app.get('/ruangan/:id', async (c) => {
   return c.json({
     status: true,
     data: ruang,
+  });
+});
+
+app.get('/kelas', async (c) => {
+  const kelas = await prisma.kelaspraktikum.findMany({
+    skip: c.req.query('offset') ? Number(c.req.query('offset')) : 0,
+    take: c.req.query('limit') ? Number(c.req.query('limit')) : 10,
+    where: {
+      nama: {
+        search: c.req.query('q') ? String(c.req.query('q')) : undefined,
+      },
+    },
+    select: {
+      id: true,
+      nama: true,
+      kuota_praktikan: true,
+      asisten: {
+        select: {
+          id: true,
+          nama: true,
+          nim: true,
+        },
+      },
+      matakuliahpraktikum: {
+        select: {
+          id: true,
+          nama: true,
+          kode: true,
+        },
+      },
+    },
+  });
+
+  return c.json({
+    status: true,
+    data: kelas,
+  });
+});
+
+app.post('/kelas', async (c) => {
+  const json = await c.req.json<{
+    nama: string;
+    kuota_praktikan: number;
+    mata_kuliah_praktikum: number;
+  }>();
+
+  await prisma.kelaspraktikum.create({
+    data: {
+      nama: json.nama,
+      kuota_praktikan: json.kuota_praktikan,
+      mata_kuliah_praktikum_id: json.mata_kuliah_praktikum,
+    },
+  });
+
+  return c.json(
+    {
+      status: true,
+    },
+    201,
+  );
+});
+
+app.put('/kelas', async (c) => {
+  const json = await c.req.json<{
+    kelas_id: number;
+    nama: string;
+    kuota_praktikan: number;
+    mata_kuliah_praktikum: number;
+  }>();
+  const kelas = await prisma.kelaspraktikum.findFirst({
+    where: {
+      id: json.kelas_id,
+    },
+  });
+  if (!kelas) {
+    return c.json({ status: false, message: 'Kelas not found' }, 404);
+  }
+  await prisma.kelaspraktikum.update({
+    where: {
+      id: json.kelas_id,
+    },
+    data: {
+      nama: json.nama,
+      kuota_praktikan: json.kuota_praktikan,
+      mata_kuliah_praktikum_id: json.mata_kuliah_praktikum,
+    },
+  });
+
+  return c.json(
+    {
+      status: true,
+    },
+    202,
+  );
+});
+
+app.delete('/kelas', async (c) => {
+  const json = await c.req.json<{
+    kelas_id: number;
+  }>();
+  const kelas = await prisma.kelaspraktikum.findFirst({
+    where: {
+      id: json.kelas_id,
+    },
+  });
+  if (!kelas) {
+    return c.json({ status: false, message: 'Kelas not found' }, 404);
+  }
+  await prisma.kelaspraktikum.delete({
+    where: {
+      id: json.kelas_id,
+    },
+  });
+
+  return c.json(
+    {
+      status: true,
+    },
+    202,
+  );
+});
+
+app.get('/kelas/:id', async (c) => {
+  const kelasId = +c.req.param('id');
+  const kelas = await prisma.kelaspraktikum.findFirst({
+    where: {
+      id: kelasId,
+    },
+    select: {
+      id: true,
+      nama: true,
+      kuota_praktikan: true,
+      asisten: {
+        select: {
+          id: true,
+          nama: true,
+          nim: true,
+        },
+      },
+      matakuliahpraktikum: {
+        select: {
+          id: true,
+          nama: true,
+          kode: true,
+        },
+      },
+    },
+  });
+  if (!kelas) {
+    return c.json({ status: false, message: 'Kelas not found' }, 404);
+  }
+  return c.json({
+    status: true,
+    data: kelas,
   });
 });
 
