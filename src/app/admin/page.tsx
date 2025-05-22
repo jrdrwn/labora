20506c0c24544172067a3d1441974a49d024770e
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-05-22 16:43:10 +0700

    feat: add Radix UI components for avatar and dialog, and implement admin login page layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,136 @@
+import Header from '@/components/layout/shared/header';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Shapes, UserPen, Users, Warehouse } from 'lucide-react';
+import Link from 'next/link';
+
 export default function AdminPage() {
   return (
     <>
-      <header className="fixed inset-x-0 top-0 z-99999 h-16 border-b-2">
-        <div className="flex h-full w-full items-center justify-between px-4">
-          <h1 className="text-2xl font-bold">Labora</h1>
-          <div className="flex items-center gap-4">
-            <a
-              href="/admin/login"
-              className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-            >
-              Login
-            </a>
+      <Header
+        suffix={'Labora'}
+        prefix={
+          <>
+            <Link href={'/admin/event'}>
+              <Button variant={'secondary'} className="rounded-full">
+                Event
+              </Button>
+            </Link>
+            <Button variant={'outline'} className="rounded-full px-2">
+              Admin
+              <Avatar className="size-6">
+                <AvatarImage
+                  src={
+                    'https://images.unsplash.com/photo-1733621770053-9b1a5f433a8c'
+                  }
+                />
+                <AvatarFallback>LB</AvatarFallback>
+              </Avatar>
+            </Button>
+          </>
+        }
+        menus={[
+          {
+            title: 'Overview',
+            href: '/admin/overview',
+          },
+          {
+            title: 'Ruangan',
+            href: '/admin/ruangan',
+          },
+          {
+            title: 'Mata Kuliah',
+            href: '/admin/mata-kuliah',
+          },
+          {
+            title: 'Kelas',
+            href: '/admin/kelas',
+          },
+          {
+            title: 'Jadwal',
+            href: '/admin/jadwal',
+          },
+          {
+            title: 'Asisten',
+            href: '/admin/asisten',
+          },
+          {
+            title: 'Praktikan',
+            href: '/admin/praktikan',
+          },
+          {
+            title: 'Laporan',
+            href: '/admin/laporan',
+          },
+        ]}
+      />
+      <main className="h-svh pt-16">
+        <section className="m-8">
+          <div className="grid grid-cols-4 gap-8">
+            <Card>
+              <CardHeader className="relative pr-16">
+                <div className="absolute top-0 right-6 flex items-center justify-center rounded-full bg-secondary p-3">
+                  <Users className="size-6 text-secondary-foreground" />
+                </div>
+                <CardTitle>Praktikan</CardTitle>
+                <CardDescription>
+                  Jumlah praktikan yang terdaftar
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="mr-1 text-6xl font-medium">100</span>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="relative pr-16">
+                <div className="absolute top-0 right-6 flex items-center justify-center rounded-full bg-secondary p-3">
+                  <UserPen className="size-6 text-secondary-foreground" />
+                </div>
+                <CardTitle>Asisten</CardTitle>
+                <CardDescription>
+                  Jumlah asisten yang lulus seleksi
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="mr-1 text-6xl font-medium">100</span>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="relative pr-16">
+                <div className="absolute top-0 right-6 flex items-center justify-center rounded-full bg-secondary p-3">
+                  <Shapes className="size-6 text-secondary-foreground" />
+                </div>
+                <CardTitle>Kelas</CardTitle>
+                <CardDescription>
+                  Jumlah kelas praktikum yang dibuat
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="mr-1 text-6xl font-medium">100</span>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="relative pr-16">
+                <div className="absolute top-0 right-6 flex items-center justify-center rounded-full bg-secondary p-3">
+                  <Warehouse className="size-6 text-secondary-foreground" />
+                </div>
+                <CardTitle>Ruangan</CardTitle>
+                <CardDescription>Jumlah ruangan yang tersedia</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="mr-1 text-6xl font-medium">100</span>
+              </CardContent>
+            </Card>
           </div>
-        </div>
-      </header>
+        </section>
+      </main>
     </>
   );
 }
---
+++ src/components/layout/shared/header.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { cn } from '@/lib/utils';
+import { Biohazard, Menu } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
+
+interface Props {
+  suffix?: ReactNode;
+  menus: {
+    title: string;
+    href: string;
+  }[];
+  prefix?: ReactNode;
+}
+
+export default function Header({ suffix, menus, prefix }: Props) {
+  const pathname = usePathname();
+
+  return (
+    <header className="fixed inset-x-0 top-0 z-99999 h-16 border-b-2">
+      <div className="flex h-full w-full items-center justify-between px-4">
+        <div className="flex items-center gap-4">
+          <Link
+            href="/"
+            scroll={false}
+            className="flex items-center gap-2"
+            prefetch={false}
+          >
+            <Button className="rounded-full">
+              <Biohazard />
+              <span className="">{suffix}</span>
+            </Button>
+          </Link>
+          <NavigationMenu>
+            <NavigationMenuList className="hidden items-center gap-2 text-sm font-medium lg:flex lg:gap-2 xl:gap-4">
+              {menus.map((menu) => (
+                <NavigationMenuItem key={menu.title}>
+                  <NavigationMenuLink
+                    className={navigationMenuTriggerStyle({
+                      className: cn(
+                        'bg-transparent',
+                        pathname === menu.href
+                          ? 'underline underline-offset-2'
+                          : '',
+                      ),
+                    })}
+                    active={pathname === menu.href}
+                    asChild
+                  >
+                    <Link href={menu.href}>{menu.title}</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex items-center gap-4">
+          {prefix}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="rounded-full lg:hidden"
+              >
+                <Menu className="size-5 text-foreground/70" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-4 lg:hidden">
+              <SheetTitle>Menu</SheetTitle>
+              <NavigationMenu className="items-start">
+                <NavigationMenuList className="grid gap-4">
+                  {menus.map((menu) => (
+                    <NavigationMenuItem key={menu.title}>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                        active={pathname === menu.href}
+                        asChild
+                      >
+                        <Link href={menu.href}>{menu.title}</Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+}
