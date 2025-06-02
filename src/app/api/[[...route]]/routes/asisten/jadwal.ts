e7ca3e1548a396b2de2c3e4f095f22cbf8a6727a
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-06-03 01:01:18 +0700

    feat: implement validation for jadwal update route and refactor related logic

--- src/app/api/[[...route]]/routes/asisten/jadwal.ts
+++ src/app/api/[[...route]]/routes/asisten/jadwal.ts
@@ -1,121 +1,149 @@
 import prisma from '@db';
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { z } from 'zod';
 
 import { JWTPayload } from '../../types';
 import generateSingleDate from '../../utils/generateSingleDate';
 
 export const jadwal = new Hono().basePath('/jadwal');
 
-jadwal.put('/', async (c) => {
-  const jwtPayload = c.get('jwtPayload') as JWTPayload;
-  const json = await c.req.json<{
-    jadwal_id: number;
-    ruang_id?: number;
-    tanggal_mulai: Date;
-    jam_mulai: string;
-    jam_selesai: string;
-  }>();
+jadwal.put(
+  '/',
+  zValidator(
+    'json',
+    z.object({
+      where: z.object({
+        jadwal_id: z.number().int().positive(),
+      }),
+      update: z.object({
+        ruang_id: z.number().int().positive().optional(),
+        tanggal_mulai: z.string(),
+        jam_mulai: z
+          .string()
+          .refine((val) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(val), {
+            message: 'Invalid time format',
+          }),
+        jam_selesai: z
+          .string()
+          .refine((val) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(val), {
+            message: 'Invalid time format',
+          }),
+      }),
+    }),
+  ),
+  async (c) => {
+    const jwtPayload = c.get('jwtPayload') as JWTPayload;
+    const json = c.req.valid('json');
 
-  const jadwal = await prisma.jadwalpraktikum.findFirst({
-    where: {
-      id: json.jadwal_id,
-      kelaspraktikum: {
-        asisten_id: jwtPayload.sub,
+    const jadwal = await prisma.jadwal.findFirst({
+      where: {
+        id: json.where.jadwal_id,
+        kelas: {
+          asisten_id: jwtPayload.sub,
+        },
       },
-    },
-  });
-  if (!jadwal) {
-    return c.json({ status: false, message: 'Jadwal not found' }, 404);
-  }
+    });
+    if (!jadwal) {
+      return c.json({ status: false, message: 'Jadwal not found' }, 404);
+    }
 
-  const ruang = await prisma.ruang.findFirst({
-    where: {
-      id: json.ruang_id,
-    },
-  });
-  if (!ruang) {
-    return c.json({ status: false, message: 'Ruang not found' }, 404);
-  }
+    if (jadwal.is_dilaksanakan) {
+      return c.json(
+        { status: false, message: 'Jadwal sudah dilaksanakan' },
+        400,
+      );
+    }
 
-  const date = generateSingleDate(
-    json.tanggal_mulai,
-    json.jam_mulai,
-    json.jam_selesai,
-  );
-
-  const endDate = new Date(date.selesai);
-  const startDate = new Date(date.mulai);
-  startDate.setSeconds(startDate.getSeconds() + 1);
-  endDate.setSeconds(endDate.getSeconds() - 1);
-
-  const existingBookings = await prisma.jadwalpraktikum.findMany({
-    where: {
-      id: {
-        not: json.jadwal_id,
+    const ruang = await prisma.ruangan.findFirst({
+      where: {
+        id: json.update.ruang_id,
       },
-      ruang_id: json.ruang_id,
-      mulai: {
-        lte: endDate,
+    });
+    if (!ruang) {
+      return c.json({ status: false, message: 'Ruang not found' }, 404);
+    }
+
+    const date = generateSingleDate(
+      json.update.tanggal_mulai as unknown as Date,
+      json.update.jam_mulai,
+      json.update.jam_selesai,
+    );
+
+    const endDate = new Date(date.selesai);
+    const startDate = new Date(date.mulai);
+    startDate.setSeconds(startDate.getSeconds() + 1);
+    endDate.setSeconds(endDate.getSeconds() - 1);
+
+    const existingBookings = await prisma.jadwal.findMany({
+      where: {
+        id: {
+          not: json.where.jadwal_id,
+        },
+        ruang_id: json.update.ruang_id,
+        mulai: {
+          lte: endDate,
+        },
+        selesai: {
+          gte: startDate,
+        },
       },
-      selesai: {
-        gte: startDate,
+    });
+
+    if (existingBookings.length > 0) {
+      return c.json({ status: false, message: 'Jadwal sudah ada' }, 400);
+    }
+
+    // cek jika event sudah ada
+    const event = await prisma.event.findFirst({
+      where: {
+        jenis: 'praktikum',
+        mulai: {
+          lte: date.mulai,
+        },
+        selesai: {
+          gte: date.selesai,
+        },
       },
-    },
-  });
+    });
 
-  if (existingBookings.length > 0) {
-    return c.json({ status: false, message: 'Ruang sudah terdaftar' }, 400);
-  }
+    if (!event) {
+      return c.json({ status: false, message: 'Jadwal diluar event' }, 404);
+    }
 
-  // cek jika event sudah ada
-  const event = await prisma.event.findFirst({
-    where: {
-      jenis: 'praktikum',
-      mulai: {
-        lte: date.mulai,
+    let data;
+    if (json.update.ruang_id) {
+      data = {
+        ruang_id: json.update.ruang_id,
+        mulai: date.mulai,
+        selesai: date.selesai,
+      };
+    } else {
+      data = {
+        mulai: date.mulai,
+        selesai: date.selesai,
+      };
+    }
+
+    await prisma.jadwal.update({
+      where: {
+        id: json.where.jadwal_id,
       },
-      selesai: {
-        gte: date.selesai,
+      data,
+    });
+
+    return c.json(
+      {
+        status: true,
       },
-    },
-  });
-
-  if (!event) {
-    return c.json({ status: false, message: 'Jadwal diluar event' }, 404);
-  }
-
-  let data;
-  if (json.ruang_id) {
-    data = {
-      ruang_id: json.ruang_id,
-      mulai: date.mulai,
-      selesai: date.selesai,
-    };
-  } else {
-    data = {
-      mulai: date.mulai,
-      selesai: date.selesai,
-    };
-  }
-
-  await prisma.jadwalpraktikum.update({
-    where: {
-      id: json.jadwal_id,
-    },
-    data,
-  });
-
-  return c.json(
-    {
-      status: true,
-    },
-    202,
-  );
-});
+      202,
+    );
+  },
+);
 
 jadwal.get('/single/:id', async (c) => {
   const jadwalId = +c.req.param('id');
-  const jadwal = await prisma.jadwalpraktikum.findFirst({
+  const jadwal = await prisma.jadwal.findFirst({
     where: {
       id: jadwalId,
     },
@@ -123,14 +151,14 @@ jadwal.get('/single/:id', async (c) => {
       id: true,
       mulai: true,
       selesai: true,
-      status: true,
-      ruang: {
+      is_dilaksanakan: true,
+      ruangan: {
         select: {
           id: true,
           nama: true,
         },
       },
-      kelaspraktikum: {
+      kelas: {
         select: {
           id: true,
           nama: true,
@@ -154,7 +182,7 @@ jadwal.get('/single/:id', async (c) => {
   });
 });
 
-jadwal.get('/batch', async (c) => {
+jadwal.get('/', async (c) => {
   const jwtPayload = c.get('jwtPayload') as JWTPayload;
   const ruangId = c.req.query('ruangId') ? +c.req.query('ruangId')! : undefined;
   const kelasId = c.req.query('kelasId') ? +c.req.query('kelasId')! : undefined;
@@ -164,31 +192,31 @@ jadwal.get('/batch', async (c) => {
 
   if (me) {
     where = {
-      kelaspraktikum: {
+      kelas: {
         asisten_id: jwtPayload.sub,
       },
     };
   } else {
     where = {
       ruang_id: ruangId,
-      kelas_praktikum_id: kelasId,
+      kelas_id: kelasId,
     };
   }
 
-  const jadwal = await prisma.jadwalpraktikum.findMany({
+  const jadwal = await prisma.jadwal.findMany({
     where,
     select: {
       id: true,
       mulai: true,
       selesai: true,
-      status: true,
-      ruang: {
+      is_dilaksanakan: true,
+      ruangan: {
         select: {
           id: true,
           nama: true,
         },
       },
-      kelaspraktikum: {
+      kelas: {
         select: {
           id: true,
           nama: true,
@@ -199,7 +227,7 @@ jadwal.get('/batch', async (c) => {
               nim: true,
             },
           },
-          matakuliahpraktikum: {
+          mata_kuliah: {
             select: {
               id: true,
               kode: true,
