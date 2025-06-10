d2908c287ad4367fc6412bbeffd7e86d6985c92b
Jordi Irawan <jordirwn@gmail.com>
2025-06-10 12:41:10 +0700

    fix: add conditional rendering for jadwal_selanjutnya to handle absence gracefully

--- src/app/praktikan/page.tsx
+++ src/app/praktikan/page.tsx
@@ -135,19 +135,25 @@ export default async function PraktikanPage() {
               <CardDescription>Jadwal praktikum selanjutnya</CardDescription>
             </CardHeader>
             <CardContent className="flex flex-col gap-2">
-              <div className="flex items-center gap-3">
-                <Calendar className="size-4 text-primary" />
-                <span className="text-muted-foreground">
-                  {extractDate(overview.jadwal_selanjutnya.mulai)}
-                </span>
-              </div>
-              <div className="flex items-center gap-3">
-                <Timer className="size-4 text-primary" />
-                <span className="text-muted-foreground">
-                  {extractTime(overview.jadwal_selanjutnya.mulai)} -{' '}
-                  {extractTime(overview.jadwal_selanjutnya.selesai)}
-                </span>
-              </div>
+              {overview.jadwal_selanjutnya ? (
+                <>
+                  <div className="flex items-center gap-3">
+                    <Calendar className="size-4 text-primary" />
+                    <span className="text-muted-foreground">
+                      {extractDate(overview.jadwal_selanjutnya.mulai)}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <Timer className="size-4 text-primary" />
+                    <span className="text-muted-foreground">
+                      {extractTime(overview.jadwal_selanjutnya.mulai)} -{' '}
+                      {extractTime(overview.jadwal_selanjutnya.selesai)}
+                    </span>
+                  </div>
+                </>
+              ) : (
+                <>Jadwal sudah selesai</>
+              )}
             </CardContent>
           </Card>
         </div>
